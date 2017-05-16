cba5e6c984b56e4e698ee3e71dfaa797aed0d6a9
Валера Любимов <v.udav@bk.ru>
2017-05-16 21:00:10 +0300

    Add layout component

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import Button from 'react-toolbox/lib/button/Button';
+import Layout from 'react-toolbox/lib/layout/Layout';
+import Panel from 'react-toolbox/lib/layout/Panel';
+import AppBar from 'react-toolbox/lib/app_bar/AppBar';
+import IconMenu from 'react-toolbox/lib/menu/IconMenu';
+import MenuItem from 'react-toolbox/lib/menu/MenuItem';
+import MenuDivider from 'react-toolbox/lib/menu/MenuDivider';
 import theme from '../toolbox/theme.js';
 import '../toolbox/theme.css';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
@@ -8,7 +13,18 @@ class App extends Component {
 	render() {
 		return (
 			<ThemeProvider theme={theme}>
-				<Button label="Hello World!" />
+				<Layout>
+					<Panel>
+						<AppBar title='Drum Practice' />
+						<IconMenu icon='more_vert' position='topLeft' menuRipple>
+							<MenuItem value='download' icon='get_app' caption='Download' />
+							<MenuItem value='help' icon='favorite' caption='Favorite' />
+							<MenuItem value='settings' icon='open_in_browser' caption='Open in app' />
+							<MenuDivider />
+							<MenuItem value='signout' icon='delete' caption='Delete' disabled />
+						</IconMenu>
+					</Panel>
+				</Layout>
 			</ThemeProvider>
 		);
 	}
